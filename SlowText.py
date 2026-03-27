a6cfd8ee6bfa7d683b7d60a373c1ca0d966e1618
Emanuel <moreirapedrinie@gmail.com>
2026-03-27 14:18:24 -0300

    teste

--- SlowText.py
+++
@@ -1,9 +0,0 @@
-import time
-import sys
-
-def slowprint(text, speed=0.02):
-    for char in text:
-        sys.stdout.write(char)
-        sys.stdout.flush()
-        time.sleep(speed)
-    print()
