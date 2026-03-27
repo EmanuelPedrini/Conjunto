3afa68d03e9185ba991828a3b6fa2715639c3dd1
Emanuel <moreirapedrinie@gmail.com>
2026-03-27 14:19:39 -0300

    teste30

---
+++ (BRUTAL!!) RetroQuest/Characters_Data.py
@@ -0,0 +1,12 @@
+from Character import character
+from Skill_Data import healing, fireball
+
+Knight = character("Knight","She","her",5, 2, 5, 0, 3, 3, 10, 0, 0, 0, [healing], [], 0 )
+Mage = character("Mage","he", "His", 3, 3, 3, 1, 5, 5, 10, 0, 0, 0, [fireball], [], 5)
+Vampire= character("Vampire", "She", "her", 4, 2, 6, -1, 2, 2, 10, 20, 0, 0, [], [], 0 )
+Penintent= character("Penitent", "He", "his", 2, 1, 8, -3, 1, 1, 9, 0, 5, 0, [], [], 0)
+Bull=character("Bull", "She", "her", 4, 2, 6, 0, 1, 2, 10, 0, 0, 5, [], [], 0)
+Huntress=character("Huntress", "She", "Her", 2, 6, 4, 4, 3, 3, 10, 0, 0, 0, [], [], 10 )
+Jammy=character("Jammy", "He", "His", 3, 3, 3, 6, 6, 3, 10, 0, 0, 0, [], [], 15)
+
+allcharacters=[Knight, Mage, Vampire, Penintent, Bull, Huntress, Jammy]
