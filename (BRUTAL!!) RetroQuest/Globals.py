b0a430f8ec3b9c54efa0c297c7fe8d377009a45a
Emanuel <moreirapedrinie@gmail.com>
2026-03-28 14:30:55 -0300

    oi

--- (BRUTAL!!) RetroQuest/Globals.py
+++ (BRUTAL!!) RetroQuest/Globals.py
@@ -1,5 +1,5 @@
 
 globaldanger = 1
 globaldangercalc = (0.5*(globaldanger + 1))
-globaldangermathsoftcap= (globaldanger*5/((globaldanger*5)+15))
+globaldangermathsoftcap= (1+(globaldanger*5/((globaldanger*5)+15)))
 gamerunning = 1
--- (BRUTAL!!) RetroQuest/Itens_Data.py
+++ (BRUTAL!!) RetroQuest/Itens_Data.py
@@ -1,24 +1,24 @@
 from Itens import item
 #weapons
-rustysword=item(name="Rusty sword", slot="Weapon",  bonus={"dex": 1}, atkform="melee")
-iron_sword = item(name="Iron Sword", slot="Weapon", bonus={"strg": 1}, atkform="melee")
+rustysword=item(name="Rusty sword", slot="Weapon",  bonus={"dex": 2}, atkform="melee")
+iron_sword = item(name="Iron Sword", slot="Weapon", bonus={"strg": 2}, atkform="melee")
 butchers_cleaver = item(name="Butcher's Cleaver", slot="Weapon", bonus={"vit": 2}, atkform="melee")
 
 #lista com todas as weapons
 todasaarmas=[rustysword, iron_sword, butchers_cleaver]
 
 #armors
-torn_clothes = item("Torn Clothes", "Armor", 1)
-leather_armor = item("Leather Armor", "Armor", 3)
-chainmail = item("Chainmail", "Armor", 5)
+torn_clothes = item("Torn Clothes", "Armor", bonus={"vit": 2} )
+leather_armor = item("Leather Armor", "Armor", bonus={"vit": 2} )
+chainmail = item("Chainmail", "Armor", bonus={"vit": 2})
 
 #lista com todas as armaduras
 todasasarmors=[torn_clothes, leather_armor, chainmail]
 
 #acsessories
-old_ring = item("Old Ring", "Accessory", 1)
-mana_pendant = item("Mana Pendant", "Accessory", 2)
-lucky_charm = item("Lucky Charm", "Accessory", 3)
+old_ring = item("Old Ring", "Accessory",bonus={"strg": 1})
+mana_pendant = item("Mana Pendant", "Accessory",bonus={"intel": 2} )
+lucky_charm = item("Lucky Charm", "Accessory", bonus={"luck": 2})
 
 #lista com todos
 todososacesssorios=[old_ring, mana_pendant, lucky_charm]
